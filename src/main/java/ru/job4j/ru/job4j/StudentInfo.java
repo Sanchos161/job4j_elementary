5ac387b37ea7ec83a0e16d78742a1bf55ffe250f
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-10-23 13:20:51 +0300

    4.1. Разбор первой программы. [#159033 #218003]

--- src/main/java/ru/job4j/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/ru/job4j/StudentInfo.java
@@ -2,8 +2,8 @@ package ru.job4j.ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println(Александр Тимошенко);
-        System.out.println(06.11 .1986);
+        System.out.println("Александр Тимошенко");
+        System.out.println("06.11.1986");
     }
 }
 
