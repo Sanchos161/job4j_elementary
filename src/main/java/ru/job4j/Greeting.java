705bc5b59abdd7660a04cf850afeef173dca841c
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-10-31 12:40:36 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! But I am a newbie";
+        int year = 2012;
+
+        System.out.println(idea + year);
+    }
+}
