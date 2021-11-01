b386ccca4a57090c713df643c96d918ff3c1c182
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-01 20:24:41 +0300

    9.1. String [#237407]

--- src/main/java/ru/job4j/Greeting.java
+++ src/main/java/ru/job4j/Greeting.java
@@ -2,9 +2,12 @@ package ru.job4j;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java! But I am a newbie";
-        int year = 2012;
+        String idea = "I like Java";
+        String line  = "But I am a newbie";
+        int year = 2021;
+        String separate = " ";
+        String id = idea + separate + line + separate + year;
 
-        System.out.println(idea + year);
+        System.out.println(id);
     }
 }
