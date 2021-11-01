f770245fc950335e2f769d3ccea9ebfeefb3b015
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-01 22:23:37 +0300

    8. Стороны прямоугольника. [#137735 #222511]

--- src/main/java/ru/job4j/Greeting.java
+++ src/main/java/ru/job4j/Greeting.java
@@ -3,7 +3,6 @@ package ru.job4j;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java";
-        System.out.println(idea);
         String line  = "But I am a newbie";
         int year = 2021;
         String separate = " ";
