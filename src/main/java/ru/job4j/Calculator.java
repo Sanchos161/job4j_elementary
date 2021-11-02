2378fda82ccab191e2a591fca108454b32a28298
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-02 20:53:53 +0300

    9.1. String [#237407 #222785]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,6 +1,8 @@
 package ru.job4j;
 
-public class Calculator { public static void main(String[] args) {
+public class Calculator {
+
+    public static void main(String[] args) {
     int one = 1;
     int two = 2;
     int six = 6;
