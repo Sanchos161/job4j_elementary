9d05fd5aeff335c0cd4a39e6d6451c4fa07b0c23
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-08 12:15:43 +0300

    3. Тесты для определения расстояния между точками. [#159459 #226084]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = -1;
+        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         return rsl;
     }
 
@@ -11,17 +11,17 @@ public class Point {
         System.out.println("result (0, 0) to (2, 0) " + result);
     }
 
-    public static void when10to20then3(String[] args) {
+    public static void when10to20then2(String[] args) {
         double result = Point.distance(1, 0, 2, 0);
         System.out.println("result (1, 0) to (2, 0) " + result);
     }
 
-    public static void when10to21then4(String[] args) {
+    public static void when10to21then1(String[] args) {
         double result = Point.distance(1, 0, 2, 1);
         System.out.println("result (1, 0) to (2, 1) " + result);
     }
 
-    public static void when21to20then5(String[] args) {
+    public static void when21to20then1(String[] args) {
         double result = Point.distance(2, 1, 2, 0);
         System.out.println("result (2, 1) to (2, 0) " + result);
     }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class PointTest {
     @Test
     public void when00to20then2() {
-        double expected = -1;
+        double expected = 2;
         int x1 = 0;
         int y1 = 0;
         int x2 = 2;
@@ -17,8 +17,8 @@ public class PointTest {
 
     @Test
 
-    public void when10to20then3() {
-        double expected = -1;
+    public void when10to20then1() {
+        double expected = 1;
         int x1 = 1;
         int y1 = 0;
         int x2 = 2;
@@ -29,8 +29,8 @@ public class PointTest {
 
     @Test
 
-    public void when10to21then4() {
-        double expected = -1;
+    public void when10to21then1() {
+        double expected = 1.4142135623730951;
         int x1 = 1;
         int y1 = 0;
         int x2 = 2;
@@ -41,8 +41,8 @@ public class PointTest {
 
     @Test
 
-    public void when21to20then5() {
-        double expected = -1;
+    public void when21to20then1() {
+        double expected = 1;
         int x1 = 2;
         int y1 = 1;
         int x2 = 2;
