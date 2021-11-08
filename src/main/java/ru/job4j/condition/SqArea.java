f8486779725f2741cc4af11b28be77f03a06c76b
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-08 12:40:08 +0300

    4. Тесты для стороны прямоугольника. [#159460]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -12,4 +12,14 @@ public class SqArea {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
     }
+
+    public static void whenP6K1Square1(String[] args) {
+        double result1 = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 1, s = 1, real = " + result1);
+    }
+
+    public static void whenP3K1Square1(String[] args) {
+        double result1 = SqArea.square(3, 1);
+        System.out.println(" p = 3, k = 1, s = 1, real = " + result1);
+    }
 }
--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,17 +1,36 @@
 package ru.job4j.condition;
-
+import static ru.job4j.math.MathFunction.*;
 import org.junit.Assert;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
 public class SqAreaTest {
 
     @Test
-    public void whenP6K2Square2() {
-        int expected = 2;
+    public void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP6K1Square2() {
+        double expected = 2.25;
         int p = 6;
-        double k = 2;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP3K1Square3() {
+       double expected = 0.5625;
+        int p = 3;
+        double k = 1;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
