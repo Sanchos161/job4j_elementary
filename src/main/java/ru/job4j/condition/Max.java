03952d74465441a3976b3e8a108a54f1334bb784
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-15 12:45:32 +0300

    4.2. Тернарное сравнение [#189]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,15 +2,15 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = true;
-        int result =  left > right ? left : right;
+        boolean condition = left > right;
+        int result =  condition ? left : right;
         return result;
 
     }
 
     public static int summation(int first, int second) {
-        boolean condition = true;
-        int result = first > second ? first : second;
+        boolean condition = first > second;
+        int result = condition ? first : second;
         return result;
 
     }
