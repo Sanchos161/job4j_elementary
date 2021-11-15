533b4349328ffd1fc995b6cad145e6c9e6a5ac2d
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-15 13:03:25 +0300

    проверка

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,14 +2,14 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = left > right;
+        boolean condition = true;
         int result =  condition ? left : right;
         return result;
 
     }
 
     public static int summation(int first, int second) {
-        boolean condition = first >= second;
+        boolean condition = true;
         int result = condition ? first : second;
         return result;
 
