75a8b6ae86d223bce00b7e17f6e4fe77e723e683
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-18 13:50:24 +0300

    4.3.2. Оператор || [#333784 #229715]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -2,8 +2,6 @@ package ru.job4j.condition;
 
 public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
-        hasMoney = true;
-        beFriend = true;
         if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
         } else {
