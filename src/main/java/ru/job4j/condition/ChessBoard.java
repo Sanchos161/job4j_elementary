ebfc4b328426cda6d1122ccfed23c55f6de18fcf
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-11-23 17:22:06 +0300

    4.3.3. Логическое отрицание ! [#334072 #229798]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -3,11 +3,6 @@ package ru.job4j.condition;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
 
-        int rsl = 0;
-        if (x1 == x2 || y1 == y2) {
-            rsl = Math.abs(x2 - x1);
-            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
-        }
-        return rsl;
+         if (abs(x1 - x2) == abs(y1 - y2));
     }
 }
