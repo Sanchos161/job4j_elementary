0260ed5894b2d97448b7f1693bf29f36d444ee59
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2021-12-17 18:01:22 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,9 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = amount + (amount * (percent / 100));
+            amount = amount * (1 + (percent / 100)) - salary;
             year++;
+
         }
         return year;
     }
