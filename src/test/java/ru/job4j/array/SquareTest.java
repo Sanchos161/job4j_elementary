d2c6cda09529cf0f1369fa3c815fa6b00e38d3b4
Sanchos161 <92474082+Sanchos161@users.noreply.github.com>
2022-01-12 10:47:00 +0300

    6.0.4. Заполнить массив степенями чисел. [#33488]

--- src/test/java/ru/job4j/array/SquareTest.java
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -17,7 +17,7 @@ public class SquareTest {
     public void whenBound5Then014916() {
         int bound = 5;
         int[] result = Square.calculate(bound);
-        int[] expected = new int[] {0, 1, 4, 9, 16,};
+        int[] expected = new int[] {0, 1, 4, 9, 16};
         Assert.assertArrayEquals(expected, result);
     }
 }
